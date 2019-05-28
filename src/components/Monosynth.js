1b0261fd8367102a377ecfea53c488951a759742
mvaughan <magnus.vaughan@gmail.com>
2019-05-28 21:55:38 +0100

    Added envelope ADSR sliders

--- src/components/Monosynth.js
+++ src/components/Monosynth.js
@@ -6,10 +6,9 @@ class Monosynth extends React.Component {
     constructor(props) {
         super(props);
         this.playSound = this.playSound.bind(this);
-    }
-
-    playSound() {
-        var synth = new Tone.Synth({
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {  
+          synth: {
             oscillator: {
               type: 'triangle8'
             },
@@ -19,14 +18,44 @@ class Monosynth extends React.Component {
               sustain: 0.4,
               release: 8
             }
-          }).toMaster()
-          
-        synth.triggerAttackRelease('C2', '8n')
+          }
+        };
+    }
+
+    handleChange (event) {
+      var eventValue = event.target.value;
+      var eventName = event.target.name;
+      var eventParent = event.target.getAttribute('data-parent');
+      console.log(eventName, eventValue, eventParent);
+      this.setState(prevState => {
+          const synth = {...prevState.synth};
+          synth[eventParent][eventName] = parseFloat(eventValue);
+          return { synth }
+      })
+    
+    }
+
+    playSound() {
+        var synth = new Tone.Synth(this.state.synth).toMaster();
+        synth.triggerAttackRelease('C4', '8n');
     }
 
     render() {
       return (
-          <button onClick={this.playSound}>Click me</button>
+          <div className="synth-wrapper">
+            <button onClick={this.playSound}>Click me</button>
+            <div>
+              <input onChange={this.handleChange} type="range" data-parent="envelope" id="attack" name="attack" min="0" max="1" step="0.1" value={this.state.synth.envelope.attack} />
+              <label for="attack">Attack</label>
+              <input onChange={this.handleChange} type="range" data-parent="envelope" id="decay" name="decay" min="0" max="100" value={this.state.synth.envelope.decay} />
+              <label for="decay">Decay</label>
+              <input onChange={this.handleChange} type="range" data-parent="envelope" id="sustain" name="sustain" min="0" max="10" step="1" value={this.state.synth.envelope.sustain} />
+              <label for="sustain">Sustain</label>
+              <input onChange={this.handleChange} type="range" data-parent="envelope" id="release" name="release" min="0" max="10" step="1" value={this.state.synth.envelope.release} />
+              <label for="release">Release</label>
+            </div>
+          </div>
+
       )
     }
 
